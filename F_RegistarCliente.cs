74bb7a461771af2abe35835596bcaef2d6ccab5e
Ezze191 <sgonzale191@gmail.com>
2024-08-01 17:22:43 -0600

    se programo la opcion de registro de usuarios

--- F_RegistarCliente.cs
+++ F_RegistarCliente.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -29,15 +31,43 @@ namespace Proyecto_POO
 
         private void bt_registrarCliente_Click(object sender, EventArgs e)
         {
+            Conexion_BaseDatos conectarBD = new Conexion_BaseDatos();
+            conectarBD.EstablecerConexion();
+
             string nombre = tb_nombre.Text;
-            string apellido = tb_apellido.Text; 
+            string apellido = tb_apellido.Text;
+            double cashback = 0;
+            int vecesvisitadas = 0;
 
+            if(nombre == string.Empty || apellido == string.Empty)
+            {
+                MessageBox.Show("TIENES QUE LLENAR LOS DATOS REQUERIDOS");
+            }
+            else
+            {
+                ingresardatos();
+            }
 
-
-            MessageBox.Show("El cliente se ah registrado Correctamente");
-            Eleccion_de_consumo eleccion = new Eleccion_de_consumo();
-           eleccion.Show();
-           this.Hide();
+            void ingresardatos() { 
+            string consulta = "insert into cliente values (" + "'" + nombre + "'" + "," + "'" + apellido + "'" + "," + cashback + "," + vecesvisitadas + ")" + ";";
+            MySqlCommand comando = new MySqlCommand(consulta,conectarBD.conexion);
+            comando.CommandTimeout = 60;
+            MySqlDataReader reader;
+            try
+            {
+                reader = comando.ExecuteReader();
+                reader.Close();
+                MessageBox.Show("CLIENTE REGISTRADO CORRECTAMENTE");
+                Eleccion_de_consumo eleccion = new Eleccion_de_consumo();
+                eleccion.Show();
+                this.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("AH OCURRIDO UN ERROR AL REGISTRAR AL CLIENTE");
+                
+            }
+            }
         }
 
         private void bt_back_Click(object sender, EventArgs e)
