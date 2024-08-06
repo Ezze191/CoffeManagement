209b7e1f55372910fbdc26b397e95af1d5bce4e6
ERIK FERNANDO HERNANDEZ AYALA <20170131@uttcampus.edu.mx>
2024-08-05 20:08:40 -0600

    n

--- F_RegistarCliente.Designer.cs
+++ F_RegistarCliente.Designer.cs
@@ -85,7 +85,7 @@
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(545, 492);
+            this.label2.Location = new System.Drawing.Point(592, 492);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(181, 51);
             this.label2.TabIndex = 4;
