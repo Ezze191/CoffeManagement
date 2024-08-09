7f2645afecb6246e04e2f26dfc445e442e92d372
ERIK FERNANDO HERNANDEZ AYALA <20170131@uttcampus.edu.mx>
2024-08-08 21:12:28 -0600

    En memoria de Darielito51

--- VentanaAdmin.Designer.cs
+++ VentanaAdmin.Designer.cs
@@ -84,6 +84,8 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(192, 22);
             this.textBox1.TabIndex = 9;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
             // 
             // label5
             // 
--- VentanaAdmin.cs
+++ VentanaAdmin.cs
@@ -30,7 +30,13 @@ namespace Proyecto_POO
 
         private void VentanaAdmin_Load(object sender, EventArgs e)
         {
+            // Configurar el texto inicial y el color
+            textBox1.Text = "Ejemplo con 4 digitos: 42.01";
+            textBox1.ForeColor = Color.Gray;
 
+            // Asignar los eventos Enter y Leave
+            textBox1.Enter += textBox1_Enter;
+            textBox1.Leave += textBox1_Leave;
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -45,23 +51,92 @@ namespace Proyecto_POO
             double precio = Convert.ToDouble(textBox1.Text);
             switch (opcion)
             {
+
               case 0:
-                    //programar precio americano
-                    actualizarPrecio actualizarPrecio = new actualizarPrecio();
-                    actualizarPrecio.actualizarPrecios("cafe americano", precio);
+                    try
+                    { //programar precio americano
+                        actualizarPrecio actualizarPrecio = new actualizarPrecio();
+                        actualizarPrecio.actualizarPrecios("cafe americano", precio);
+                        MessageBox.Show("Se actualizo correctamente el precio del cafe americano");
+                    }
+                    catch 
+                    {
+                        MessageBox.Show("Error al actualizar el precio del cafe americano");
+
+                    }
+                   
+                   
                     break;
                 case 1:
-                    //pro
+                    try
+                    { //programar precio americano
+                        actualizarPrecio actualizarPrecio = new actualizarPrecio();
+                        actualizarPrecio.actualizarPrecios("capuccino", precio);
+                        MessageBox.Show("Se actualizo correctamente el precio del capuccino");
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Error al actualizar el precio del capuccino");
+
+                    }
+
+                    break;
+                case 2:
+                    try
+                    { //programar precio americano
+                        actualizarPrecio actualizarPrecio = new actualizarPrecio();
+                        actualizarPrecio.actualizarPrecios("latte frio", precio);
+                        MessageBox.Show("Se actualizo correctamente el precio del latte frio");
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Error al actualizar el precio del latte frio");
+
+                    }
+
+
                     break;
                 case 3:
+                    try
+                    { //programar precio americano
+                        actualizarPrecio actualizarPrecio = new actualizarPrecio();
+                        actualizarPrecio.actualizarPrecios("macciato", precio);
+                        MessageBox.Show("Se actualizo correctamente el precio del Macciato");
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Error al actualizar el precio del Macciato");
 
+                    }
 
                     break;
-                case 4:
+
+                    case 4:
+                    try
+                    { //programar precio americano
+                        actualizarPrecio actualizarPrecio = new actualizarPrecio();
+                        actualizarPrecio.actualizarPrecios("moka", precio);
+                        MessageBox.Show("Se actualizo correctamente el precio del Moka");
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Error al actualizar el precio del Moka");
+
+                    }
 
                     break;
-
                     case 5:
+                    try
+                    { //programar precio americano
+                        actualizarPrecio actualizarPrecio = new actualizarPrecio();
+                        actualizarPrecio.actualizarPrecios("esspreso", precio);
+                        MessageBox.Show("Se actualizo correctamente el precio del Espresso");
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Error al actualizar el precio del Espresso");
+
+                    }
 
                     break;
 
@@ -72,6 +147,24 @@ namespace Proyecto_POO
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Ejemplo con 4 digitos: 42.01")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                textBox1.Text = "Ejemplo con 4 digitos: 42.01";
+                textBox1.ForeColor = Color.Gray;
+            }
         }
     }
 }
